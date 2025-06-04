9379b97d05af9f8cdad50659580f712143a199dd
jacob <jacob.a.fox@gmail.com>
2025-06-03 19:03:05 -0700

    majorUpgrades.ContentUpgrades.MinorTweaks/bugfixes
    4.147.404
    added textures to wolf pet

--- src/main/java/goat/minecraft/minecraftnew/subsystems/pets/perks/Alpha.java
+++ src/main/java/goat/minecraft/minecraftnew/subsystems/pets/perks/Alpha.java
@@ -4,13 +4,13 @@ import goat.minecraft.minecraftnew.subsystems.pets.PetManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.projectiles.Projectile;
 
 import java.util.*;
 
