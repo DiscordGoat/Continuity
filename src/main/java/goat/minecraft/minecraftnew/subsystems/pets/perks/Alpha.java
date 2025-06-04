3c5d883b5172dff02a827fad7b8cb3d82418491c
jacob <jacob.a.fox@gmail.com>
2025-06-03 19:10:38 -0700

    majorUpgrades.ContentUpgrades.MinorTweaks/bugfixes
    4.147.404

--- src/main/java/goat/minecraft/minecraftnew/subsystems/pets/perks/Alpha.java
+++ src/main/java/goat/minecraft/minecraftnew/subsystems/pets/perks/Alpha.java
@@ -11,7 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.projectiles.Projectile;
+
 
 import java.util.*;
 
