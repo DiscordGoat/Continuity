32c7a72026f40f8f1766b7eb7cc3aeaa6eecfd3a
DiscordGoat <69318868+DiscordGoat@users.noreply.github.com>
2025-06-30 03:12:50 -0700

    Expand natures wrath flow

--- src/main/java/goat/minecraft/minecraftnew/subsystems/armorsets/FlowType.java
+++ src/main/java/goat/minecraft/minecraftnew/subsystems/armorsets/FlowType.java
@@ -1,21 +1,48 @@
 package goat.minecraft.minecraftnew.subsystems.armorsets;
 
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Types of Flow effects used for armor sets.
  */
 public enum FlowType {
-    NATURES_WRATH(Material.IRON_AXE);
+    NATURES_WRATH(
+            Arrays.asList(
+                    Material.OAK_LOG,
+                    Material.OAK_LEAVES,
+                    Material.BIRCH_LOG,
+                    Material.BIRCH_LEAVES,
+                    Material.GRASS_BLOCK,
+                    Material.VINE,
+                    Material.OAK_SAPLING,
+                    Material.SUNFLOWER,
+                    Material.DANDELION,
+                    Material.LILY_PAD
+            ),
+            Particle.SPORE_BLOSSOM_AIR
+    );
 
-    private final Material material;
+    private static final Random RANDOM = new Random();
+    private final List<Material> materials;
+    private final Particle particle;
 
-    FlowType(Material material) {
-        this.material = material;
+    FlowType(List<Material> materials, Particle particle) {
+        this.materials = materials;
+        this.particle = particle;
     }
 
     public ItemStack createItem() {
-        return new ItemStack(material);
+        Material mat = materials.get(RANDOM.nextInt(materials.size()));
+        return new ItemStack(mat);
+    }
+
+    public Particle getParticle() {
+        return particle;
     }
 }
