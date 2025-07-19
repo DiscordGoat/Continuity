54e0a73a29fcc4c3c8758be2bb7a4a16d1b0df6f
DiscordGoat <69318868+DiscordGoat@users.noreply.github.com>
2025-07-19 16:28:48 -0700

    Add player stats command and resistance manager

--- src/main/java/goat/minecraft/minecraftnew/MinecraftNew.java
+++ src/main/java/goat/minecraft/minecraftnew/MinecraftNew.java
@@ -52,6 +52,7 @@ import goat.minecraft.minecraftnew.subsystems.villagers.VillagerWorkCycleManager
 import goat.minecraft.minecraftnew.utils.commands.DiscsCommand;
 import goat.minecraft.minecraftnew.utils.commands.MeritCommand;
 import goat.minecraft.minecraftnew.utils.commands.SkillsCommand;
+import goat.minecraft.minecraftnew.utils.commands.StatsCommand;
 import goat.minecraft.minecraftnew.utils.commands.AuraCommand;
 import goat.minecraft.minecraftnew.utils.commands.ToggleCustomEnchantmentsCommand;
 import goat.minecraft.minecraftnew.utils.developercommands.*;
@@ -514,6 +515,9 @@ public class MinecraftNew extends JavaPlugin implements Listener {
         SkillsCommand skillsCommand = new SkillsCommand(xpManager);
         this.getCommand("skills").setExecutor(skillsCommand);
         getServer().getPluginManager().registerEvents(skillsCommand, this);
+        StatsCommand statsCommand = new StatsCommand(this);
+        this.getCommand("stats").setExecutor(statsCommand);
+        getServer().getPluginManager().registerEvents(statsCommand, this);
         new SetSkillLevelCommand(this, xpManager);
 
         SkillTreeManager.init(this);
