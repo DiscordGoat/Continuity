8b3186763baddda52a32e519106ce7c21dcdb310
jacob <jacob.a.fox@gmail.com>
2025-07-25 23:17:33 -0700

    15.375.634

--- src/main/java/goat/minecraft/minecraftnew/subsystems/farming/CropCountManager.java
+++ src/main/java/goat/minecraft/minecraftnew/subsystems/farming/CropCountManager.java
@@ -72,6 +72,7 @@ public class CropCountManager {
         save();
         handleRewards(player, crop, c, total);
 
+
     }
 
     private void handleRewards(Player player, Material crop, int cropCount, int total) {
